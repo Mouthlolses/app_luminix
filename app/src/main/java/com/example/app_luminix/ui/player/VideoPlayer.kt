0ae004534ad336bb26ec31d8928d5d6d563262cc
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-12 16:35:29 -0300

    initial_concept_playervideo

---
+++ app/src/main/java/com/example/app_luminix/ui/player/VideoPlayer.kt
@@ -0,0 +1,60 @@
+package com.example.app_luminix.ui.player
+
+import android.content.Context
+import android.net.Uri
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+
+// # Componente que lida com ExoPlayer
+
+//Aqui é onde configuramos o ExoPlayer para reproduzir o vídeo.
+
+@Composable
+fun VideoPlayer(videoUrl: String) {
+
+    val context = LocalContext.current
+    val exoPlayer = rememberExoPlayer(context, videoUrl)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.navigationBars.asPaddingValues())
+    ) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                PlayerView(ctx).apply {
+                    player = exoPlayer
+                    useController = true
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberExoPlayer(context: Context, videoUrl: String): ExoPlayer {
+    return remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(Uri.parse(videoUrl)))
+            prepare()
+            play()
+            playWhenReady = true
+            pause()
+        }
+    }
+
+}
